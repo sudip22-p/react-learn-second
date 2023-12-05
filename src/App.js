21e3c21626b06b20a7c92574751fe95fb4f70fb5
sudip <paudelsudip134@gmail.com>
2023-12-05 21:25:02 +0545

    components,props,prototypes,other-basics

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
+// let xyz="i have a";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <>
+    // //this is js fragment
+    // </>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React with sud 20:30:14
+    //     </a>
+    //   </header>
+    // </div>//return vitra vayeko html jsx 
+    <>
+      {/* <nav>
+        <ul>
+          <li>Home</li>
+          <li>Contact</li>
+          <li>Help</li>
+          <li>Login</li>
+        </ul>
+      </nav>
+      <div className="container">
+        <h1>{xyz} CAR</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit unde cumque delectus nulla deleniti adipisci fugit distinctio exercitationem, cum doloribus labore magnam laborum non commodi dignissimos sapiente eum repellendus maxime repellat blanditiis.</p>
+      </div> */}
+    <Navbar title="TextCounts-2001"/>
+    </>
   );
 }
 
